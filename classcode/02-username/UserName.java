d8a6037434ecee8093c7576ed2321928b990d2b9
Joshua Hans <jhans@schools.nyc.gov>
2022-10-19 08:49:02 -0400

    can generate random CAPITAL letter

--- classcode/02-username/UserName.java
+++ classcode/02-username/UserName.java
@@ -17,6 +17,8 @@ public class UserName {
     System.out.println("Hello " + getInitial(firstName) + "." + getInitial(lastName) + ".");
     System.out.println(favNum);
 
+    generatePassword(8);
+    
     s.close();
   } // end main method
 
@@ -36,10 +38,24 @@ public class UserName {
     // need a loop of len "length"
 
     // pull a random number from range (65-90 CAPS) (48-57 #s) (97-122 lower)
+    int min = 65;
+    int max = 90;
+
+    int rand = (int)(Math.random() * (max - min + 1) + min);
 
     // convert int to char (c =(char)i;)
+    char c = (char)rand;
+
+    System.out.println(rand + " " + c);
 
     // append char to the String password (password = password + c)
     return password;
   }
+
+
+
+
+
+
+
 } // end class
