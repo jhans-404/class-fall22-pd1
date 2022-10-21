4d751828747863341726179115a4aaad0c0ff5de
Joshua Hans <jhans@schools.nyc.gov>
2022-10-21 11:18:49 -0400

    makes random password of letters and nums

--- classcode/02-username/UserName.java
+++ classcode/02-username/UserName.java
@@ -5,6 +5,7 @@ public class UserName {
     // create Scanner object
     Scanner s = new Scanner(System.in);
 
+    /*
     // get String input from user
     System.out.print("Enter your first name: ");
     String firstName = s.nextLine();
@@ -16,9 +17,12 @@ public class UserName {
     // test output
     System.out.println("Hello " + getInitial(firstName) + "." + getInitial(lastName) + ".");
     System.out.println(favNum);
+    */
 
-    generatePassword(8);
-    
+    System.out.print("How long should the password be? ");
+    int len = s.nextInt();
+    System.out.println(generatePassword(len));
+
     s.close();
   } // end main method
 
@@ -36,26 +40,41 @@ public class UserName {
     String password = "";
 
     // need a loop of len "length"
+    for(int i = 0; i < length; i++) {
+      // pull a random number from range (65-90 CAPS) (48-57 #s) (97-122 lower)
 
-    // pull a random number from range (65-90 CAPS) (48-57 #s) (97-122 lower)
-    int min = 65;
-    int max = 90;
+      int min, max;
 
-    int rand = (int)(Math.random() * (max - min + 1) + min);
+      if(i % 3 == 0) {
+        // uppercase letter range
+        min = 65;
+        max = 90;
+      } // end if statement
+      else if(i % 3 == 1) {
+        // lowercase letter range
+        min = 97;
+        max = 122;
+      } // end else if statement
+      else {
+        // number range
+        min = 48;
+        max = 57;
+      } // end else statement
 
-    // convert int to char (c =(char)i;)
-    char c = (char)rand;
+      // once we have the range, choose random integer
+      int rand = (int)(Math.random() * (max - min + 1) + min);
 
-    System.out.println(rand + " " + c);
+      // convert int to char (c =(char)i;)
+      char c = (char)rand;
 
-    // append char to the String password (password = password + c)
+      // update password to add-on the random character
+      password += c; // equivalent to password = password + c
+
+      // don't need this test statement anymore
+      // System.out.println(rand + " " + c);
+    } // end for loop
+
     return password;
-  }
-
-
-
-
-
-
+  } // end generatePassword method
 
 } // end class
